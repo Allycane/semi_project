9bbacea46a68a33905ccbe53861a82f2d2cc4366
tkdudsh <syn_1234@naver.com>
2026-05-26 16:08:47 +0900

    [syn] hotData connect

--- tennisfolio-front/src/component/Hot.jsx
+++ tennisfolio-front/src/component/Hot.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import productHot from '../data/productHot.js';
 import Products from './Products.jsx';
+import { getHotProducts } from '../util/product.js';
 
 function Hot() {
   let [tennisHot] = useState(productHot);
   let [clicked] = useState("hot");
+  let [hotProducts, setHotProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchHotProducts = async()=>{
+      const result =await getHotProducts();
+      setHotProducts(result);
+    };
+      fetchHotProducts();    
+    }
+
+  , []);
 
   return (
     <div className="container hotItems" style={{ margin: "150px auto 150px", maxWidth: "1600px" }}>
       <h3 style={{ fontWeight: "700" }}>요즘 핫해요</h3>
       <div className="row row-cols-2 row-cols-md-3 row-cols-xxl-4">
-        {tennisHot.map((item, index) => (
+        {hotProducts.map((item, index) => (
           <Products
             key={index}
             clicked={clicked}
--- tennisfolio-server/app.js
+++ tennisfolio-server/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from "cors";
 import cartRouter from "./routes/carts.js";
+import productRouter from "./routes/products.js";
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/carts', cartRouter);
+app.use('/products', productRouter);
 
 app.listen(4000, () => {
   console.log(`Server is running on port 4000`);
